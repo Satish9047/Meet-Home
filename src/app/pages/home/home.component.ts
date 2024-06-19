5a26fbae4b770d75e3909206436f77aa4c10f344
Satish9047 <satishprajapati930@gmail.com>
2024-06-19 22:28:14 +0545

    add house list with responsiveness + search UI input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchComponent } from '../../features/search/search.component';
+import { HouseListComponent } from '../../features/house-list/house-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SearchComponent, HouseListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
