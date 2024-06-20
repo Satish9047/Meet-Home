0f3f08b5ccb11b5d501eef6f8b15234fd0ed2d67
Satish9047 <satishprajapati930@gmail.com>
2024-06-20 08:35:46 +0545

    add data + services

--- src/app/core/services/house.service.ts
+++ src/app/core/services/house.service.ts
@@ -1,27 +1,13 @@
 import { Injectable } from '@angular/core';
-import { IHouse } from '../interface/app';
+import { houses } from '../data/homes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HouseService {
-  private houses: IHouse[] = [
-    {
-      id: 1,
-      house: 'Villa',
-      price: 1000000,
-      location: 'Byasi-2, Bhaktapur',
-      area: '2000 sq. ft',
-      kitchen: 1,
-      bedrooms: 1,
-      bathrooms: 1,
-      washrooms: 2,
-      imageUrl: '',
-      totalFloor: 0,
-      available: false,
-      description: '',
-    },
-  ];
-
+  private houses = houses;
   constructor() {}
+  getAllHouses() {
+    return this.houses;
+  }
 }
