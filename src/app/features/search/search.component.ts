6e89983e751892a66a6faae32ced143dc7509f96
Satish9047 <satishprajapati930@gmail.com>
2024-06-24 12:54:09 +0545

    add reactive form in search feature

--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
-
+  searchForm: FormGroup;
+  constructor() {
+    this.searchForm = new FormGroup({
+      searchText: new FormControl(''),
+    });
+  }
+  handleSearch() {
+    console.log('hello search');
+    console.log(this.searchForm.value);
+  }
 }
