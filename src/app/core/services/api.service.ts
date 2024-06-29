be1826636081f169ef3d4259ec8e9d021fb6459c
Satish9047 <satishprajapati930@gmail.com>
2024-06-29 22:11:53 +0545

    add login and register method for the apiService

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  baseUrl = 'http://localhost:3000/api/v1';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getLogin(email: string, password: string) {
+    return this.http.post(`${this.baseUrl}/login`, {
+      email,
+      password,
+    });
+  }
+  getRegister(email: string, password: string) {
+    return this.http.post(`${this.baseUrl}/register`, {
+      email,
+      password,
+    });
+  }
 }
