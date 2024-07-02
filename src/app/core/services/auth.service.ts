02d053da8c94e5f83caeb46feb1795c3d330f41b
Satish9047 <satishprajapati930@gmail.com>
2024-07-02 08:55:26 +0545

    setup for auth

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {
+export class AuthService {
   baseUrl = 'http://localhost:3000/api/v1';
 
   constructor(private http: HttpClient) {}
 
   getLogin(email: string, password: string) {
-    return this.http.post(`${this.baseUrl}/login`, {
+    this.http.post(`${this.baseUrl}/login`, {
       email,
       password,
     });
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor() {
+  constructor(private authService: AuthService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
@@ -24,6 +25,8 @@ export class LoginComponent {
   }
 
   onSubmit() {
+    const { email, password } = this.loginForm.value;
+    this.authService.getLogin(email, password);
     console.log(this.loginForm.value);
   }
 }
