c2ac0765f552f54e117095ce426fae136c8e38c8
Satish9047 <satishprajapati930@gmail.com>
2024-07-02 13:21:58 +0545

    add login functionality to login component + used @if instead of *ngIf

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,19 +2,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-// import { AuthService } from '../../core/services/auth.service';
+import { AuthService } from '../../core/services/auth.service';
+import { LoadingSpinnerComponent } from '../../shared/component/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, LoadingSpinnerComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  isLoading = false;
 
-  constructor() {
+  constructor(private authService: AuthService) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
@@ -24,9 +26,22 @@ export class LoginComponent {
     });
   }
 
+  //login USING AUTHSERVICE FOR LOGIN
   onSubmit() {
-    // const { email, password } = this.loginForm.value;
-    // this.authService.getLogin(email, password);
-    // console.log(this.loginForm.value);
+    const { email, password } = this.loginForm.value;
+    this.isLoading = true;
+    this.authService.getLogin(email, password).subscribe({
+      next: (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log(error);
+        this.isLoading = false;
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
   }
 }
