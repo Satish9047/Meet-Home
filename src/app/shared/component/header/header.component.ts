a687c35a9bbfbb0f9ecbf36850c77078174190d3
Satish9047 <satishprajapati930@gmail.com>
2024-07-05 12:30:14 +0545

    add email to header when logged in

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { Subscription } from 'rxjs';
+import { User } from '../../../core/model/user.model';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +14,14 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription = new Subscription();
   isAuthenticated = false;
+  user: User | null = null;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.userSub = this.authService.user.subscribe((user) => {
       this.isAuthenticated = !user ? false : true; //or we can do !!user
+      this.user = user;
     });
   }
 
