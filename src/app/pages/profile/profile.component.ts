ed29445bb56e62eac829955ff015fa678d8b3ded
Satish9047 <satishprajapati930@gmail.com>
2024-07-06 21:26:57 +0545

    update profile page add guard + and subscribe to the user observable

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { User } from '../../core/model/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   userDetail: string = 'satish@gmail.com';
+  user: User | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+  ngOnInit() {
+    this.authService.user.subscribe((user) => {
+      if (!user) {
+        this.router.navigate(['/login']);
+      }
+      this.user = user;
+      console.log(this.user, 'from profile');
+    });
+  }
 }
