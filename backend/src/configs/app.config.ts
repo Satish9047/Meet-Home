aaa295922297c3aaa4666584efc73c5f91d13357
Satish9047 <satishprajapati930@gmail.com>
2024-07-14 21:23:16 +0545

    add jet + bcrypt + password hashing + token generater + password comparation methods

--- backend/src/configs/app.config.ts
+++ backend/src/configs/app.config.ts
@@ -4,6 +4,9 @@ dotenv.config();
 const config = {
   port: Number(process.env.PORT) || 3000,
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/auth',
+  SALT_ROUND: Number(process.env.SALT_ROUND) || 10,
+  JWT_TOKEN_SECRET: process.env.JWT_TOKEN_SECRET || 'jwt-token-secret-satish',
+  JWT_EXIPRES_IN: process.env.JWT_EXIPRES_IN || '1h',
 };
 
 export default config;
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,6 +1,10 @@
-import { timeStamp } from 'console';
+import bcript from 'bcrypt';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 
+import config from '../configs/app.config';
+
+//USER SCHEMA
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -19,4 +23,34 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+//HASHING THE PASSWORD BEFORE SAVING IT
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  this.password = await bcript.hash(this.password, config.SALT_ROUND);
+  next();
+});
+
+//COMPARING THE PASSWORD METHOD
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcript.compare(password, this.password);
+};
+
+//GENETRATING JWT TOKEN
+userSchema.methods.generateJwtToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
+    config.JWT_TOKEN_SECRET,
+    { expiresIn: config.JWT_EXIPRES_IN },
+  );
+};
+
+//USER MODEL
 export const User = mongoose.model('User', userSchema);
