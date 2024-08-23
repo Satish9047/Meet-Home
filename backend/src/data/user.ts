a0a3144df873d0596f48f622948fcbccc8be4660
Satish9047 <satishprajapati930@gmail.com>
2024-08-23 10:03:09 +0545

    update user data info

--- backend/src/data/user.ts
+++ backend/src/data/user.ts
@@ -1,4 +1,4 @@
 export const users = [
   { email: 'admin@gmail.com', password: 'password123', isAdmin: true },
-  { email: 'user@gamilcom', password: 'password123', isAdmin: false },
+  { email: 'user@gmailcom', password: 'password123', isAdmin: false },
 ];
